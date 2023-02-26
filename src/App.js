287a8132e12e70b78392e38633e916b9dc79571b
github_vikash20020908 <vikash20020908@gmail.com>
2023-02-26 15:54:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Resturant from "./Component/basics/Resturant";
+
+
+const App  = ()  => {
+
+  return   <Resturant/>
+};
+export default App;
---
+++ src/Component/basics/Resturant.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./Resturant.css";
+import Menu from "./menuAPI.js";
+import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+console.log(uniqueList);
+
+const Resturant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+  console.log(setMenuList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
+    </>
+  );
+};
+
+export default Resturant;
